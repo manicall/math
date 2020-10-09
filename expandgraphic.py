411cb6b4be20cd4ba9e83df151499ff23edf9569
max <makskms1@gmail.com>
2020-10-09 23:08:07 +1000

    4

---
+++ expandgraphic.py
@@ -0,0 +1,52 @@
+import sys
+import sympy as sp
+import numpy as np
+from PyQt5 import QtCore, QtWidgets
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg, NavigationToolbar2QT as NavigationToolbar
+from matplotlib.figure import Figure
+
+class MplCanvas(FigureCanvasQTAgg):
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        super(MplCanvas, self).__init__(fig)
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        QtWidgets.QMainWindow.__init__(self, parent)
+        toolbar = NavigationToolbar(self.sc, self)
+        self.sc = MplCanvas()
+        # Create toolbar, passing canvas as first parament, parent (self, the MainWindow) as second.
+        self.grid = QtWidgets.QGridLayout()
+        self.grid.setAlignment(QtCore.Qt.AlignHCenter)
+        layout = QtWidgets.QVBoxLayout()
+        #layout.addWidget(toolbar)
+        layout.addWidget(self.sc)
+        layout.addLayout(self.grid)
+        # Create a placeholder widget to hold our toolbar and canvas.
+        widget = QtWidgets.QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+        #self.add_grafic(start, end, count, f, lambda x: x * 0)
+        self.show()
+    def add_grafic(self, start, end, count, *args):
+        x = np.linspace(start, end, count)
+        for i in args:
+            self.sc.axes.plot(x, i(x))
+    def add_grid(self, str, x, y):
+        self.grid.addWidget(QtWidgets.QLabel(str), x, y)
+
+
+
+app = QtWidgets.QApplication(sys.argv)
+w = MainWindow()
+x = sp.Symbol('x')
+y = sp.log(x, 10) - 7 / (2 * x + 6)  # функция
+f = sp.lambdify(x, y)
+start = 1
+end = 5
+count = 100
+w.add_grafic(start, end, count, f, lambda x: x * 0)
+w.add_grid('text',0,0)
+
+
+app.exec()
