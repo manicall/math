893bf75871baf69e27ada6287805d16001340602
max <makskms1@gmail.com>
2020-11-26 01:00:41 +1000

    мое задание

--- math6.py
+++ math6.py
@@ -1,9 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = 0.263
-ix = [0.05, 0.1, 0.17, 0.25, 0.30, 0.36]
-iy = [0.050042, 0.100335, 0.171657, 0.255342, 0.309336, 0.376403]
+x = 0.896
+ix = [0.68, 0.73, 0.80, 0.88, 0.93, 0.99]
+iy = [0.80866, 0.89492, 1.02964, 1.20966, 1.34087, 1.52368]
 n = len(ix)
 list_of_division = []
 for i in range(n):
@@ -12,6 +12,7 @@ for i in range(n):
         list_of_difference[i] = x - ix[i]
     Di = (np.prod(list_of_difference))
     list_of_division.append(iy[i]/Di)
+    print(list_of_difference)
     print("Di:", str(np.round(Di, 10)).ljust(12, " "), "iy[i]/Di[i]:", np.round(iy[i]/Di, 5))
 
 print(list_of_division)
