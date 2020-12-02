85ed01eecc0b94b7c2518dcb284fcd8745dfa93e
max <makskms1@gmail.com>
2020-12-03 01:42:04 +1000

    another variant

--- math7.py
+++ math7.py
@@ -7,22 +7,31 @@ a = 0.8
 b = 1.4
 y = '1 / sqrt(2 * x ** 2 + 3)'
 #пример
-#a = 0.7
-#b = 1.3
-#y = '1 / sqrt(2 * x ** 2 + 0.3)'
+a = 0.7; b = 1.3
+y = '1 / sqrt(2 * x ** 2 + 0.3)'
 f = smp.lambdify('x', y)
 d2y = smp.diff(y, 'x', 'x')
+#d2y = smp.simplify(d2y)
+
 d2f = smp.lambdify('x', d2y)
 M2 = max(np.abs(d2f(np.linspace(a,b,1000))))
-print('max:', M2)
 
-n = round((b - a) ** 3 * M2 / (12 * 0.0005) ** 2)
+#print(d2y)
+#l1 = lambda x: np.sqrt(2)*(x**2 - 0.075)
+#l2 = lambda x: (x**2 + 0.15)**(5/2)
+#M2 = l1(b)/l2(a)
+
+n = round(np.sqrt((b - a) ** 3 * M2 / (12 * 0.0005))) + 1
 h = (b - a) / n
 x = [a + i*h for i in range(n + 1)]
 iy = [f(x[i]) for i in range(n + 1)]
 list1 = [iy[i] for i in range(1, n)]
 list1.insert(0, (iy[0] + iy[-1]) / 2)
 I = h*sum(list1)
+
+print('max:', M2)
+print('n:', n)
+print('h:', h)
 print('Ответ:', I)
 print("проверка:", integrate.quad(smp.lambdify('x', y), a, b)[0],
       '(проверку осуществляет встроенная функция python, вычисляющая интеграл)')
@@ -33,14 +42,6 @@ n = 8
 a = 0.4
 b = 1.2
 y = 'sqrt(x)*cos(x**2)'
-#пример
-#a = 1.2
-#b = 1.6
-#y = 'sin(2*x-2.1)/(x**2+1)'
-
-
-
-
 f = smp.lambdify('x', y)
 
 h = (b - a) / n
@@ -55,7 +56,7 @@ for i in range(1, n):
         list1.append(2*f(x[i]))
 list1.append(f(x[n]))
 
-I = h/3 * sum(list1)
+I = h / 3 * sum(list1)
 
 delt1_y = [np.round(iy[i + 1] - iy[i], 4) for i in range(len(iy)-1)]
 delt2_y = [np.round(delt1_y[i + 1] - delt1_y[i], 4) for i in range(len(delt1_y)-1)]
