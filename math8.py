0971f043c6779c82bf75eee0f5d31bc89f378303
max-pc <makskms@gmail.com>
2020-12-17 13:27:09 +1000

    sweet

--- math8.py
+++ math8.py
@@ -1,11 +1,12 @@
+import copy
 import numpy as np
 import sympy as sp
-import copy
+
 
 h = 0.1
-dy = '0.2 * x + y ** 2'
+dy = '0.2*x+y**2'
 f = sp.lambdify(['x', 'y'], dy)
-x = np.linspace(0, 1, 11)
+x = np.round(np.linspace(0, 1, 11),1)
 y = [0.1]
 
 for i in range(0, 3):
@@ -21,12 +22,16 @@ for i in range(0, 3):
              )
 y1 = copy.copy(y)
 y2 = copy.copy(y)
-print(y)
-for i in range(3, 10):
-    y1.append(y[i - 4] + (4 * h / 3) * (2 * f(x[i - 3], y[i - 3])
-                                        - f(x[i - 2], y[i - 2])
-                                        + f(x[i - 1], y[i - 1])))
-    y2.append(y[i - 2] + h / 3 * (f(x[i - 2], y[i - 2]) + 4 * f(x[i - 1], y[i - 1]) + f(x[i], y1[i])))
-    y.append(round(y2[i],4))
-
-print(y)
+for i in range(4, 11):
+    y.append(None)
+    y1.append(None)
+    y2.append(None)
+    y1[i] = y[i - 4] + (4 * h / 3) * (2 * f(x[i - 3], y[i - 3])
+                                      - f(x[i - 2], y[i - 2])
+                                      + 2 * f(x[i - 1], y[i - 1]))
+    y2[i] = y[i - 2] + h / 3 * (f(x[i - 2], y[i - 2]) + 4 * f(x[i - 1], y[i - 1]) + f(x[i], y1[i]))
+    y[i] = round(y2[i], 4)
+print("=======Ответ========")
+print('x'.rjust(7), 'y'.rjust(8))
+for i in range(11):
+    print(str(i).ljust(2), [str(x[i]), str(y[i]).ljust(6)])
